55feb3b4bf66fa958ad80deaf2e73babc87b2820
James <emeryj@broadinstitute.org>
2018-06-25 16:20:20 -0400

    death on all sides

--- build_docker.sh
+++ build_docker.sh
@@ -142,7 +142,6 @@ if [ -z "${IS_NOT_RUN_UNIT_TESTS}" ] ; then
     chmod -R a+w ${STAGING_ABSOLUTE_PATH}/src/test/resources
 
 	echo docker run ${REMOVE_CONTAINER_STRING} -v  ${STAGING_ABSOLUTE_PATH}:/gatksrc -v  ${STAGING_ABSOLUTE_PATH}/testJars:/jars -t ${REPO_PRJ}:${GITHUB_TAG} bash /root/run_unit_tests.sh
-#	docker run ${REMOVE_CONTAINER_STRING} -v ${STAGING_ABSOLUTE_PATH}:/gatksrc -v ~/.gradle:/root/.gradle -t ${REPO_PRJ}:${GITHUB_TAG} bash /root/run_unit_tests.sh
     docker run ${REMOVE_CONTAINER_STRING} -v  ${STAGING_ABSOLUTE_PATH}:/gatksrc -v  ${STAGING_ABSOLUTE_PATH}/testJars:/jars -t ${REPO_PRJ}:${GITHUB_TAG} bash /root/run_unit_tests.sh
 	echo " Unit tests passed..."
 fi
--- scripts/m2_cromwell_tests/run_m2_wdl.sh
+++ scripts/m2_cromwell_tests/run_m2_wdl.sh
@@ -12,7 +12,6 @@ echo "Building docker image for M2 WDL tests (skipping unit tests)..."
 #cd $WORKING_DIR/gatk/scripts/docker/
 #assume Dockerfile is in root
 echo "Building docker without running unit tests... ========="
-#HASH_TO_USE="28c3ab05f8b80de076f9c26cff59441f0ef3928e"
 cd $WORKING_DIR/gatk
 # IMPORTANT: This code is duplicated in the cnv WDL test.
 if [ ${TRAVIS_PULL_REQUEST} != false ]; then
@@ -23,13 +22,13 @@ else
   sudo bash build_docker.sh  -e ${HASH_TO_USE} -s -u -d $PWD/temp_staging/;
 fi
 echo "Docker build done =========="
-CROMWELL_JAR=/usr/local/Cellar/cromwell/32/libexec/cromwell-32.jar
 echo "Putting the newly built docker image into the json parameters"
 cd $WORKING_DIR/gatk/scripts/
-sed  -r "s/__GATK_DOCKER__/broadinstitute\/gatk\:$HASH_TO_USE/g" m2_cromwell_tests/test_m2_wdl_multi.json >$WORKING_DIR/test_m2_wdl_multi_mod.json
+sed -r "s/__GATK_DOCKER__/broadinstitute\/gatk\:$HASH_TO_USE/g" m2_cromwell_tests/test_m2_wdl_multi.json >$WORKING_DIR/test_m2_wdl_multi_mod.json
 echo "JSON FILE (modified) ======="
 cat $WORKING_DIR/test_m2_wdl_multi_mod.json
 echo "=================="
+
 # Create the tumor-only json by using the pair_list_tumor_only file
 sed -r "s/\"pair_list/\"pair_list_tumor_only/g" $WORKING_DIR/test_m2_wdl_multi_mod.json >$WORKING_DIR/test_m2_wdl_multi_mod_to.json
 cd $WORKING_DIR/
@@ -37,7 +36,7 @@ cd $WORKING_DIR/
 echo "Running M2 WDL through cromwell (T/N)"
 ln -fs $WORKING_DIR/gatk/scripts/mutect2_wdl/mutect2.wdl
 mkdir $WORKING_DIR/logs
-sudo java -jar -Dfinal_workflow_log_dir=$WORKING_DIR/logs $CROMWELL_JAR run $WORKING_DIR/gatk/scripts/mutect2_wdl/mutect2_multi_sample.wdl -i $WORKING_DIR/test_m2_wdl_multi_mod.json -m $WORKING_DIR/test_m2_wdl.metadata
+sudo java -Dfinal_workflow_log_dir=$WORKING_DIR/logs -jar $CROMWELL_JAR run $WORKING_DIR/gatk/scripts/mutect2_wdl/mutect2_multi_sample.wdl -i $WORKING_DIR/test_m2_wdl_multi_mod.json -m $WORKING_DIR/test_m2_wdl.metadata
 
 find $WORKING_DIR/logs
 
